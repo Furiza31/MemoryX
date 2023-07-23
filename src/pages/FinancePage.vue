9f8dc8acc420ec5f0bb72d5fd931e609a73e7da0
Furiza31 <hugo.wendjaneh@gmail.com>
2023-07-23 20:13:51 +0200

    Feat|Fixed(Finance): connect finance to the api | fix some bugs

--- src/pages/FinancePage.vue
+++ src/pages/FinancePage.vue
@@ -3,7 +3,13 @@
     <div style="margin: 0px">
       <q-card>
         <q-card-section>
-          <q-input v-model="bank" label="Banque" type="number" color="primary">
+          <q-input
+            v-model="bank"
+            label="Banque"
+            type="number"
+            color="primary"
+            :debounce="500"
+          >
             <template v-slot:append>
               <q-icon name="euro_symbol" />
             </template>
@@ -33,16 +39,16 @@
               label
               :label-value="saving + '€'"
               label-always
+              @change="putSaving()"
             />
           </div>
         </q-card-section>
 
         <q-card-actions align="right">
-          <q-btn color="primary" label="Ajouter une section" @click="add()" />
           <q-btn
-            color="negative"
-            label="Dépense imprévue"
-            @click="addUnexpected()"
+            color="primary"
+            label="Ajouter une section"
+            @click="addSectionDialog()"
           />
         </q-card-actions>
 
@@ -141,37 +147,94 @@
         :key="section.name"
         :section="section"
         :rest="rest"
-        @update-value="section.value = $event"
+        @update-value="updateSection(section.id, $event)"
         @delete="deleteSection(section.id)"
       />
     </div>
+    <q-dialog v-model="showAddSectionDialog" persistent>
+      <q-card style="max-width: 350px; min-width: 300px">
+        <q-card-section>
+          <div class="text-h5 text-center">{{ $t('newTodo') }}</div>
+        </q-card-section>
+        <q-form @submit="add()">
+          <q-card-section>
+            <q-input
+              v-model="newSection.name"
+              type="text"
+              :label="$t('todoName')"
+              :rules="[(val) => val.length > 0 || $t('fieldRequired')]"
+            >
+              <template v-slot:prepend>
+                <q-icon name="edit" />
+              </template>
+            </q-input>
+            <q-toggle
+              v-model="newSection.unexpected"
+              label="Inattendu"
+              color="primary"
+              :true-value="1"
+              :false-value="0"
+            />
+          </q-card-section>
+          <q-card-actions align="right">
+            <q-btn
+              :label="$t('cancel')"
+              color="negative"
+              flat
+              @click="showAddSectionDialog = false"
+            />
+            <q-btn :label="$t('add')" color="primary" flat type="submit" />
+          </q-card-actions>
+        </q-form>
+      </q-card>
+    </q-dialog>
+    <loading-component :loading="loading" />
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import FinanceCategoryComponent from 'src/components/FinanceCategoryComponent.vue';
+import LoadingComponent from 'src/components/LoadingComponent.vue';
 import Finance from 'src/models/FinanceSection';
+import { api } from 'src/boot/axios';
+import { debounce } from 'quasar';
 
 export default defineComponent({
   name: 'FinancePage',
   components: {
     FinanceCategoryComponent,
+    LoadingComponent,
   },
   data() {
     return {
       bank: 0,
       salary: 0,
       saving: 0,
+      financeId: 0,
+      loading: false,
+      debounce: debounce,
+      startDate: new Date(),
       sections: [] as Finance[],
+      showAddSectionDialog: false,
+      newSection: {
+        id: 0,
+        name: '',
+        value: 0,
+        unexpected: 0,
+      } as Finance,
     };
   },
   watch: {
     bank() {
       if (!this.bank) this.bank = 0;
+      if (this.loading) return;
+      this.putBank();
     },
     salary() {
       if (!this.salary) this.salary = 0;
+      if (this.loading) return;
+      this.putSalary();
     },
   },
   computed: {
@@ -193,15 +256,101 @@ export default defineComponent({
     },
   },
   methods: {
+    updateSection(id: number, value: number) {
+      const section = this.sections.find((section) => section.id === id);
+      if (!section) return;
+      api
+        .put(`/finances/sections/${id}`, {
+          value: value,
+        })
+        .then(() => {
+          section.value = value;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
     deleteSection(id: number) {
-      this.sections = this.sections.filter((section) => section.id !== id);
+      api
+        .delete(`/finances/sections/${id}`)
+        .then(() => {
+          this.sections = this.sections.filter((section) => section.id !== id);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     add() {
-      // TODO: add section
+      api
+        .post('/finances/sections', {
+          name: this.newSection.name,
+          value: this.newSection.value,
+          unexpected: this.newSection.unexpected,
+          financeID: this.financeId,
+        })
+        .then((res) => {
+          this.newSection.id = res.data.id;
+          this.sections.push({ ...this.newSection });
+          this.showAddSectionDialog = false;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    addUnexpected() {
-      // TODO: add unexpected section
+    putBank() {
+      api.put('/finances/bank', { bank: this.bank }).catch((err) => {
+        console.log(err);
+      });
     },
+    putSalary() {
+      api.put('/finances/salary', { salary: this.salary }).catch((err) => {
+        console.log(err);
+      });
+    },
+    putSaving() {
+      api.put('/finances/saving', { saving: this.saving }).catch((err) => {
+        console.log(err);
+      });
+    },
+    addSectionDialog() {
+      this.newSection.name = '';
+      this.newSection.unexpected = 0;
+      this.showAddSectionDialog = true;
+    },
+  },
+  async mounted() {
+    this.loading = true;
+    let found = (await api.get('/finances/check')).data.found;
+    if (!found) {
+      await api
+        .post('/finances', {
+          bank: 0,
+          salary: 0,
+          saving: 0,
+          startDate: new Date(),
+        })
+        .then((res) => {
+          this.financeId = res.data.id;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      await api
+        .get('/finances')
+        .then((res) => {
+          this.bank = res.data.finances.bank;
+          this.salary = res.data.finances.salary;
+          this.saving = res.data.finances.saving;
+          this.startDate = res.data.finances.start_date;
+          this.financeId = res.data.finances.id;
+          this.sections = res.data.financesSections;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    this.loading = false;
   },
 });
 </script>
